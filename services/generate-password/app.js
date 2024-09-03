41de6621f842dcc1c9d9c9492db9363acd5c8763
David Seija <davidseija@gmail.com>
2024-09-03 18:09:35 -0500

    Improve generate-password service and tests

--- services/generate-password/app.js
+++ services/generate-password/app.js
@@ -1,5 +1,20 @@
+const { apiRequestData, apiResponse } = require('../../libs/api-helper');
+const { emailIsValid } = require('../../libs/validation-helper');
+const { MESSAGE } = require('./constants');
+const { sendPassword } = require('./core/send-password');
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-  };
+  const data = apiRequestData(event);
+  if (!data) return apiResponse({ message: MESSAGE.BAD_REQUEST }, 400);
+  if (!emailIsValid(data.email)) {
+    return apiResponse({ message: MESSAGE.INVALID_EMAIL }, 422);
+  }
+
+  // TODO: Create unique password and token
+  const password = '123456';
+  const token = 'SECRET_TOKEN';
+
+  sendPassword(password);
+
+  return apiResponse({ message: MESSAGE.SUCCESS, token });
 };
--- services/generate-password/app.test.js
+++ services/generate-password/app.test.js
@@ -1,14 +1,15 @@
-const { handler, sendPassword } = require('./app');
+const { handler } = require('./app');
+const SendPassword = require('./core/send-password');
+
+jest.mock('./core/send-password');
 
 describe('Generate Password Service', () => {
   const EMAIL_OK = 'email@test.com';
-  const EMAIL_EMPTY = '';
   const EMAIL_INVALID = '@invalid';
   const defaultEvent = {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: {},
   };
 
   beforeEach(() => {
@@ -18,34 +19,36 @@ describe('Generate Password Service', () => {
   describe('not accepted request', () => {
     const event = { ...defaultEvent };
 
-    it('should return statusCode 400 with empty email', async () => {
-      event.body.email = EMAIL_EMPTY;
+    it('should return statusCode 400 with wrong body', async () => {
+      event.body = {};
 
       const response = await handler(event);
       expect(response.statusCode).toEqual(400);
     });
 
-    it('should return statusCode 400 with invalid email', async () => {
-      event.body.email = EMAIL_INVALID;
+    it('should return statusCode 422 with invalid email', async () => {
+      event.body = JSON.stringify({ email: EMAIL_INVALID });
 
       const response = await handler(event);
-      expect(response.statusCode).toEqual(400);
+      expect(response.statusCode).toEqual(422);
     });
   });
 
   describe('receive email and generate password', () => {
     const event = { ...defaultEvent };
-    event.body.email = EMAIL_OK;
+    event.body = JSON.stringify({ email: EMAIL_OK });
 
     it('should call sendPassword()', async () => {
       await handler(event);
-      expect(sendPassword).toHaveBeenCalled();
+      const sendPasswordMock = jest.spyOn(SendPassword, 'sendPassword');
+      expect(sendPasswordMock).toHaveBeenCalled();
     });
 
     it('should return token', async () => {
       const response = await handler(event);
-      expect(typeof response.token).toEqual('string');
-      expect(response.token).not.toHaveLength(0);
+      const body = JSON.parse(response.body);
+      expect(typeof body.token).toEqual('string');
+      expect(body.token).not.toHaveLength(0);
     });
   });
 });
