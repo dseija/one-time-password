0f1b1154ca8fc8170632d8ab3eeba6ebdbbef3f2
David Seija <davidseija@gmail.com>
2024-09-04 18:03:53 -0500

    Add constants for generate-password service

--- services/generate-password/constants/index.js
+++ services/generate-password/constants/index.js
@@ -1,7 +1,13 @@
 const MESSAGE = {
   BAD_REQUEST: 'Error: Bad Request.',
   INVALID_EMAIL: 'Error: Email field is required and must be valid.',
+  ERROR: 'Error: Operation failed.',
   SUCCESS: 'Password generated successfully! Please check your email.',
 };
 
-module.exports = { MESSAGE };
+const CONFIG = {
+  PASSWORD_LENGTH: 8,
+  TOKEN_LENGTH: 32,
+};
+
+module.exports = { MESSAGE, CONFIG };
