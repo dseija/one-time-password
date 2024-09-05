dc1bf43f3f68ea270b9d569d88568da9ded5e6c6
David Seija <davidseija@gmail.com>
2024-09-05 13:13:38 -0500

    Complete send-password core service for generate password service

--- services/generate-password/core/send-password.js
+++ services/generate-password/core/send-password.js
@@ -1,4 +1,23 @@
-module.exports.sendPassword = (email, password) => {
-  console.log('Sending password right away!', email, password);
-  return email;
+const { sendEmail } = require('../../../libs/email-helper');
+
+module.exports.sendPassword = async (email, password) => {
+  const htmlBody = `
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <h1>${process.env.EMAIL_SUBJECT}</h1>
+        <p>Use this password to complete your signin at ${process.env.APP_NAME}:</p>
+        <h2>${password}</h2>
+      </body>
+    </html>
+  `;
+
+  const result = await sendEmail({
+    toAddresses: [email],
+    from: process.env.EMAIL_FROM,
+    subject: process.env.EMAIL_SUBJECT,
+    htmlBody,
+  });
+
+  return result;
 };
--- services/generate-password/core/send-password.test.js
+++ services/generate-password/core/send-password.test.js
@@ -1,8 +1,30 @@
+const EmailHelper = require('../../../libs/email-helper');
 const { sendPassword } = require('./send-password');
 
+jest.mock('../../../libs/email-helper');
+
 describe('sendPassword', () => {
-  it('should returns something', async () => {
+  let sendEmailMock;
+
+  beforeEach(() => {
+    sendEmailMock = jest.spyOn(EmailHelper, 'sendEmail');
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return undefined if there is an error', async () => {
+    sendEmailMock.mockImplementation(() => undefined);
     const sent = await sendPassword('test@email.com', '123456');
+    expect(sendEmailMock).toHaveBeenCalled();
+    expect(sent).toBeUndefined();
+  });
+
+  it('should not return undefined', async () => {
+    sendEmailMock.mockImplementation(() => ({}));
+    const sent = await sendPassword('test@email.com', '123456');
+    expect(sendEmailMock).toHaveBeenCalled();
     expect(sent).not.toBeUndefined();
   });
 });
